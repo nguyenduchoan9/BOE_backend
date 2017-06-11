dd3041b6cb6c4de6213c614118be5995368c190c
Phong Luu <dphong1807@gmail.com>
2017-06-11 11:56:05 +0700

    update index

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -9,7 +9,6 @@ class StatisticController < WebApplcationController
   end
 
   def make_statistic
-    @users = User.all
     respond_to do |format|
       format.html {
 
@@ -19,25 +18,25 @@ class StatisticController < WebApplcationController
         duration = params[:duration]
         if type == 'user'
           if duration == 'month'
-            render json: Order.includes(:user).group('users.username').select('users.username').references(:users).where('date_part(\'month\', orders.created_at) = date_part(\'month\', current_date)').sum(:total).first(10).to_json
+            render json: Order.order('sum_total').includes(:user).group('users.username').select('users.username').references(:users).where('date_part(\'month\', orders.created_at) = date_part(\'month\', current_date)').sum(:total).first(10).to_json
           elsif duration == 'year'
-            render json: Order.includes(:user).group('users.username').select('users.username').references(:users).where('date_part(\'year\', orders.created_at) = date_part(\'year\', current_date)').sum(:total).first(10).to_json
+            render json: Order.order('sum_total').includes(:user).group('users.username').select('users.username').references(:users).where('date_part(\'year\', orders.created_at) = date_part(\'year\', current_date)').sum(:total).first(10).to_json
           else
 
           end
         elsif type == 'order'
           if duration == 'month'
-            render json: Order.where('date_part(\'month\', orders.created_at) = date_part(\'month\', current_date)').group_by_day_of_week(:created_at, format: "%a").count.to_json
+            render json: Order.where('date_part(\'month\', orders.created_at) = date_part(\'month\', current_date)').group_by_day_of_week(:created_at, format: "%a").sum(:total).to_json
           elsif duration == 'year'
-            render json: Order.where('date_part(\'year\', orders.created_at) = date_part(\'year\', current_date)').group_by_day_of_week(:created_at, format: "%a").count.to_json
+            render json: Order.where('date_part(\'year\', orders.created_at) = date_part(\'year\', current_date)').group_by_month(:created_at, format: "%b %Y").sum(:total).to_json
           else
 
           end
         else
           if duration == 'month'
-            render json: OrderDetail.includes(:dish).group('dishes.dish_name').references(:dishes).where('date_part(\'month\', order_details.created_at) = date_part(\'month\', current_date)').sum(:quantity).to_json
+            render json: OrderDetail.includes(:dish).group('dishes.dish_name').references(:dishes).where('date_part(\'month\', order_details.created_at) = date_part(\'month\', current_date)').sum(:quantity).first(5).to_json
           elsif duration == 'year'
-            render json: OrderDetail.includes(:dish).group('dishes.dish_name').references(:dishes).where('date_part(\'year\', order_details.created_at) = date_part(\'year\', current_date)').sum(:quantity).to_json
+            render json: OrderDetail.includes(:dish).group('dishes.dish_name').references(:dishes).where('date_part(\'month\', order_details.created_at) = date_part(\'month\', current_date)').sum(:quantity).first(5).to_json
           else
 
           end
