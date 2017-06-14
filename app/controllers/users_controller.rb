62a8bdd6123e17d8e5de54ccb67ba4ac2a92831c
Phong Luu <dphong1807@gmail.com>
2017-06-14 17:35:54 +0700

    update layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,11 @@ class UsersController < WebApplcationController
 
   def update
     @user = User.find params[:user][:id]
-    @user.update_attributes(:username => params[:user][:username], :password => params[:user][:password], full_name: params[:user][:full_name], role: Role.find(params[:user][:role_id]), membership: Membership.find(params[:user][:membership_id]), email: params[:user][:email], phone: params[:user][:phone], birthdate: params[:user][:birthdate].to_date, avatar: params[:user][:image])
+    @user.update_attributes(user_params)
     redirect_to action: 'show'
   end
 
+  def user_params
+    params.require(:user).permit :id, :username, :full_name, :role_id, :membership_id, :email, :phone, :birthdate, :avatar
+  end
 end
