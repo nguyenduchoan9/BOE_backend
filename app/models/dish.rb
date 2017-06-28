5047b6a6fe56426a7bfaa97b0abf2d951404bd3d
Phong Luu <dphong1807@gmail.com>
2017-06-28 22:18:04 +0700

    update

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -18,30 +18,30 @@
 #
 
 class Dish < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    # skip_callback :save, :before, :store_picture!
+  mount_uploader :image, ImageUploader
+  # skip_callback :save, :before, :store_picture!
 
-    belongs_to :category, optional: true
-    has_many :order_details
-    has_many :price_change_histories
-    has_many :dish_discounts
-    has_many :discount_days, through: :dish_discounts
+  belongs_to :category, optional: true
+  has_many :order_details
+  has_many :price_change_histories
+  has_many :dish_discounts
+  has_many :discount_days, through: :dish_discounts
 
-    def self.search_cutlery(keysearch)
-        where('lower(dish_name) LIKE ?', "%#{keysearch}%")
+  def self.search_cutlery(keysearch)
+    where('lower(dish_name) LIKE ?', "%#{keysearch}%")
+  end
+
+  def self.search_drinking(keysearch)
+    where('lower(dish_name) LIKE ?', "%#{keysearch}%")
+  end
+
+  def self.search(term)
+    if term
+      where('lower(dish_name) LIKE ?', "%#{term.downcase}%")
     end
+  end
 
-    def self.search_drinking(keysearch)
-        where('lower(dish_name) LIKE ?', "%#{keysearch}%")
-    end
-
-    def self.search(term)
-        if term
-            where('lower(dish_name) LIKE ?', "%#{term.downcase}%")
-        end
-    end
-
-    def self.suggest_by_user(user_id)
-        ActiveRecord::Base.connection.execute('SELECT order_details.dish_id, sum(order_details.quantity) sum_quantity FROM orders, dishes, users, order_details WHERE orders.user_id = users.id AND orders.id = order_details.order_id AND dishes.id = order_details.dish_id AND user_id = 1 GROUP BY order_details.dish_id ORDER BY sum_quantity DESC FETCH FIRST 5 ROWS ONLY')
-    end
+  def self.suggest_by_user(user_id)
+    ActiveRecord::Base.connection.execute("SELECT order_details.dish_id, sum(order_details.quantity) sum_quantity FROM orders, dishes, order_details WHERE orders.id = order_details.order_id AND dishes.id = order_details.dish_id AND user_id = #{user_id} GROUP BY order_details.dish_id ORDER BY sum_quantity DESC FETCH FIRST 5 ROWS ONLY")
+  end
 end
