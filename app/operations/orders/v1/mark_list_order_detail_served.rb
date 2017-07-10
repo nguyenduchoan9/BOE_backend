038f62347c99afc05d8e92625c9294828e247fa8
nguyendhoang <hoang.nguyen@saleshood.com>
2017-07-10 08:24:42 +0700

    correct group dish in table

---
+++ app/operations/orders/v1/mark_list_order_detail_served.rb
@@ -0,0 +1,47 @@
+module Orders
+    module V1
+        class MarkListOrderDetailServed < Operation
+
+            def process
+                do_transaction!
+                # notifify each order detail to chef
+                { status: true }
+            end
+
+            private
+            def list_order_detail_id
+                params[:list_order_detail_id]
+            end
+
+            def get_list_id
+                @list_id ||= list_order_detail_id.split('_')
+            end
+
+            def do_transaction!
+                begin
+                    ActiveRecord::Base.transaction do
+                        mark_served
+                    end
+                rescue StandardError => error
+                    raise ValidateError.new(error)
+                end
+            end
+
+            def mark_served
+                if get_list_id.count > 0
+                    get_list_id.each do |id|
+                        order_detail = find_order_detail id
+                        if order_detail.cooking_status == 1
+                            order_detail.update(cooking_status: 2)
+                        end
+                    end
+                end
+
+            end
+
+            def find_order_detail id_params
+                OrderDetail.find id_params
+            end
+        end
+    end
+end
