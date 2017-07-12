d2743c714450800211b3b8a6b0c55de1163ab6e1
Phong Luu <dphong1807@gmail.com>
2017-07-12 13:46:19 +0700

    update

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -5,17 +5,17 @@ class DishesController < ApplicationController
 
   def create
     @dish = Dish.new dish_params
+    @dish.is_available = true
     @dish.save
     if params["isSocial"] == "on"
-      me = FbGraph::User.me('EAACEdEose0cBAD9N8nLuszpOzuy3wEpek93PCZBZC9y8dxC33zJDb0DDqlDNLOXMWAFJBQPZCQIg5VlPhLc32gRhgYCZAV9UnUdgRDliQM6fjv8U4VcHB76KZAVIwol2WviI12qlfdO1vrQAVlxY4gbql9uao9ZCZC0K0EwGXGgSZAMEZBr3ytZAykAP2ADal4DbJF7poQWYzwqQZDZD')
+      me = FbGraph::User.me('')
       me.feed!(
           :message => "#{@dish.description}",
           :picture => 'https://graph.facebook.com/matake/picture',
           :link => 'https://github.com/nov/fb_graph',
-          :name => 'FbGraph',
-          :description => ""
+          :name => "#{@dish.dish_name}",
+          :description => "#{@dish.created_at.to_date}"
       )
-      puts params["isSocial"]
     end
     redirect_to dishes_path
   end
