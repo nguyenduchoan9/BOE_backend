5cdf4654d30adcd508d9048d9215e791f798bfe3
Phong Luu <dphong1807@gmail.com>
2017-07-26 17:47:29 +0700

    update notification

--- app/controllers/pay_pal_controller.rb
+++ app/controllers/pay_pal_controller.rb
@@ -1,5 +1,28 @@
 class PayPalController < ApplicationController
 
+  # @payout = PayPal::SDK::REST::DataTypes::Payout.new(
+  #     {
+  #         :sender_batch_header => {
+  #             :sender_batch_id => SecureRandom.hex(8),
+  #             :email_subject => 'You have a Payout!',
+  #         },
+  #         :items => [
+  #             {
+  #                 :recipient_type => 'EMAIL',
+  #                 :amount => {
+  #                     :value => '100',
+  #                     :currency => 'USD'
+  #                 },
+  #                 :note => 'Thanks for your patronage!',
+  #                 :receiver => 'nguyendhoan9-buyer@gmail.com',
+  #                 :sender_item_id => "2014031400023",
+  #             }
+  #         ]
+  #     }
+  # )
+  #
+  # payout_batch = @payout.create
+
   def notify_webapp
     firebase = Firebase::Client.new('https://boebackend.firebaseio.com', 'FpE63dhjckc4wr6w5wGyRATrYlkEXJ3b5EJOwaGI')
     list_orderdetail_id = params[:list_orderdetail_id].split('_')
@@ -41,32 +64,6 @@ class PayPalController < ApplicationController
     end
     order.total -= total
     order.save!
-    # order_detail_id = params[:order_detail_id]
-    # order_detail = OrderDetail.find order_detail_id
-    # payment_id = order_detail.order.payment_id
-    # payment = PayPal::SDK::REST::DataTypes::Payment.find payment_id
-    # json = JSON.parse payment.to_json
-    # @sale = PayPal::SDK::REST::DataTypes::Sale.find json["transactions"][0]["related_resources"][0]["sale"]["id"]
-    # total = order_detail.quantity_not_serve * order_detail.price
-    # json = JSON.parse HTTParty.get("http://apilayer.net/api/live?access_key=088bcad87c3fd862b4793c2f535089ba").to_json
-    # rate = json["quotes"]["USDVND"].to_f
-    # usdTotal = (total.to_f / rate).round(2)
-    # @refund = @sale.refund_request({
-    #                                    :amount => {
-    #                                        :total => "#{usdTotal}",
-    #                                        :currency => "USD"
-    #                                    }
-    #                                })
-
-    # firebase = Firebase::Client.new('https://boebackend.firebaseio.com', 'FpE63dhjckc4wr6w5wGyRATrYlkEXJ3b5EJOwaGI')
-    # fcm = params[:fcm]
-    # firebase.delete("rejectedOrder/#{fcm}")
-
-    # order_detail.cooking_status = 4
-    # order_detail.order.total -= total
-    # order_detail.order.save!
-    # order_detail.save!
-
     redirect_to home_path(term: order.created_at.strftime('%d/%m/%Y'))
   end
 end
